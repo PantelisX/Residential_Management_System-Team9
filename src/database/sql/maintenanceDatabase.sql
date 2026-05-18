c2039ea4afc8a7ad634109a24580a2fe420d6620
PogMoThoin21 <aiasplsoni@gmail.com>
2026-05-19 01:07:05 +0300

    Create maintenance database and related tables

---
+++ src/database/sql/maintenanceDatabase.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE maintenance_system;
+USE maintenance_system;
+
+
+CREATE TABLE Residence 
+(
+    residence_id INT PRIMARY KEY, 
+    address VARCHAR(255) NOT NULL,
+    owner VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+
+CREATE TABLE MaintenanceTask (
+    task_id INT PRIMARY KEY,
+    residence_id INT NOT NULL,
+    status ENUM('open', 'in_progress', 'completed', 'cancelled') NOT NULL,
+    accepted BOOLEAN NOT NULL DEFAULT FALSE,
+    tech_id INT,
+    category ENUM('electrical', 'plumbing', 'hvac', 'landscaping', 'other') NOT NULL,
+    description TEXT,
+    start_date DATE,
+    end_date DATE,
+    FOREIGN KEY (residence_id) REFERENCES Residence(residence_id)
+);
+
+
+CREATE TABLE TaskHistory (
+    history_id INT PRIMARY KEY,
+    task_id INT NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    category ENUM('electrical', 'plumbing', 'hvac', 'landscaping', 'other') NOT NULL,
+    description TEXT,
+    completion_date DATE,
+    FOREIGN KEY (task_id) REFERENCES MaintenanceTask(task_id)
+);
+
+
+CREATE TABLE UserResidence 
+(
+    user_id INT,
+    residence_id INT,
+    user_role ENUM('tenant', 'owner', 'manager') NOT NULL,
+
+    PRIMARY KEY (user_id, residence_id),
+
+    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
+
+    FOREIGN KEY (residence_id) REFERENCES Residence(residence_id) ON DELETE CASCADE
+);
+
+
