7f1e89aa17d8806dc6f057e4cb412e9bce1aa700
NektSio <nektarios350@gmal.com>
2026-05-26 22:09:17 +0300

    fixed to first show the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
             <Route index element={<Navigate to="/home/residences" replace />} />
           </Route>
 
-          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
       </AuthProvider>
